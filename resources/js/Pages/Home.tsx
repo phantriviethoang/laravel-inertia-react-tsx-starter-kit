0e3bc801b39082fceb7b9b4eac65303fbe04d47b
phantriviethoang <phantriviethoang@gmail.com>
2026-01-07 11:05:22 +0700

    init

---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="h-screen flex justify-center items-center text-xl">
+            <p>
+                Edit <code>resources/js/Pages/Home.tsx</code> and save to
+                reload.
+            </p>
+        </div>
+    );
+}
